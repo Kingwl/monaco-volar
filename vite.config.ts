356199dc749b6e90f936e9add781635bbf0ba274
kingwl <kingwenlu@gmail.com>
2022-06-14 19:18:16 +0800

    fix: format

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import * as path from "path";
 
 export default defineConfig({
-  base: '',
+  base: "",
   optimizeDeps: {
     include: [
       "path-browserify",
