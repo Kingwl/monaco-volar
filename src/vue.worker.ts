8b685aa64781f32a6d797c274db8c9b443cd0091
johnsoncodehk <johnsoncodehk@gmail.com>
2023-03-04 02:43:09 +0800

    chore: format

--- src/vue.worker.ts
+++ src/vue.worker.ts
@@ -5,38 +5,36 @@ import { resolveConfig } from "@volar/vue-language-service";
 import * as volarWorker from "@volar/monaco/worker";
 
 self.onmessage = () => {
-  worker.initialize(
-    (ctx: monaco.worker.IWorkerContext) => {
-      const compilerOptions: ts.CompilerOptions = {
-        ...ts.getDefaultCompilerOptions(),
-        allowJs: true,
-        jsx: ts.JsxEmit.Preserve,
-        module: ts.ModuleKind.ESNext,
-        moduleResolution: ts.ModuleResolutionKind.NodeJs,
-      };
+  worker.initialize((ctx: monaco.worker.IWorkerContext) => {
+    const compilerOptions: ts.CompilerOptions = {
+      ...ts.getDefaultCompilerOptions(),
+      allowJs: true,
+      jsx: ts.JsxEmit.Preserve,
+      module: ts.ModuleKind.ESNext,
+      moduleResolution: ts.ModuleResolutionKind.NodeJs,
+    };
 
-      return volarWorker.createLanguageService({
-        workerContext: ctx,
-        config: resolveConfig(
-          {
-            plugins: {
-              /* volar.config.js plugins */
-            },
+    return volarWorker.createLanguageService({
+      workerContext: ctx,
+      config: resolveConfig(
+        {
+          plugins: {
+            /* volar.config.js plugins */
           },
-          ts as any,
-          compilerOptions,
-          {
-            plugins: [
-              /* tsconfig vueCompilerOptions plugins */
-            ],
-          }
-        ),
-        typescript: {
-          module: ts as any,
-          compilerOptions,
         },
-        dtsHost: volarWorker.createDtsHost("https://unpkg.com/"),
-      });
-    }
-  );
+        ts as any,
+        compilerOptions,
+        {
+          plugins: [
+            /* tsconfig vueCompilerOptions plugins */
+          ],
+        }
+      ),
+      typescript: {
+        module: ts as any,
+        compilerOptions,
+      },
+      dtsHost: volarWorker.createDtsHost("https://unpkg.com/"),
+    });
+  });
 };
