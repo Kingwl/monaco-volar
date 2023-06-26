fb6af6377b3946dabf5006ed03a4d9998d59cdda
baiwusanyu-c <740132583@qq.com>
2023-06-26 17:05:43 +0800

    feat: updated code

--- src/themes/index.ts
+++ src/themes/index.ts
@@ -1,6 +1,6 @@
 import { theme, lightTheme } from './converted'
 
-export async function loadTheme(editor: typeof import('monaco-editor-core').editor, dark = false) {
+export async function loadTheme(editor: typeof import('monaco-editor-core').editor, dark = true) {
     const themeName = 'vs-code-theme-converted';
     editor.defineTheme(themeName, dark ? theme : lightTheme);
     return themeName;
