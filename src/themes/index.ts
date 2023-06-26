a8a84ba118b3eeccdba837eda8818e5165bc5313
baiwusanyu-c <740132583@qq.com>
2023-06-26 18:11:43 +0800

    feat: updated code

--- src/themes/index.ts
+++ src/themes/index.ts
@@ -1,7 +1,11 @@
 import { theme, lightTheme } from './converted'
 
-export async function loadTheme(editor: typeof import('monaco-editor-core').editor, dark = true) {
-    const themeName = 'vs-code-theme-converted';
-    editor.defineTheme(themeName, dark ? theme : lightTheme);
-    return themeName;
+export async function loadTheme(
+  editor: typeof import('monaco-editor-core').editor,
+  dark = true) {
+    const themeNameDark = 'vs-code-theme-converted-dark';
+    editor.defineTheme(themeNameDark, theme);
+    const themeNameLight = 'vs-code-theme-converted-light';
+    editor.defineTheme(themeNameLight, lightTheme);
+    return dark ? themeNameDark : themeNameLight;
 }
