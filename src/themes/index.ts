927b78da42b9a62e1ca77b2afd145ba37ca0220e
白雾三语 <32354856+baiwusanyu-c@users.noreply.github.com>
2023-06-27 23:45:07 +0800

    updated code

--- src/themes/index.ts
+++ src/themes/index.ts
@@ -1,11 +1,9 @@
 import { theme, lightTheme } from './converted'
 
-export async function loadTheme(
-  editor: typeof import('monaco-editor-core').editor,
-  dark = true) {
+export async function loadTheme(editor: typeof import('monaco-editor-core').editor) {
     const themeNameDark = 'vs-code-theme-converted-dark';
     editor.defineTheme(themeNameDark, theme);
     const themeNameLight = 'vs-code-theme-converted-light';
     editor.defineTheme(themeNameLight, lightTheme);
-    return dark ? themeNameDark : themeNameLight;
+    return { dark: themeNameDark, light: themeNameLight }
 }
