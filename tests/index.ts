86b50de401919e8afa52ba360321b42991352c5d
Johnson Chu <johnsoncodehk@gmail.com>
2023-06-28 00:16:02 +0800

    chore: fix format

--- tests/index.ts
+++ tests/index.ts
@@ -22,30 +22,30 @@ const afterReady = (theme: string) => {
     inlineSuggest: {
       enabled: false,
     },
-    'semanticHighlighting.enabled': true,
+    "semanticHighlighting.enabled": true,
   });
 
   // Support for semantic highlighting
-  const t = (editorInstance as any)._themeService._theme
+  const t = (editorInstance as any)._themeService._theme;
   t.getTokenStyleMetadata = (
     type: string,
     modifiers: string[],
     _language: string
   ) => {
-    const _readonly = modifiers.includes('readonly')
+    const _readonly = modifiers.includes("readonly");
     switch (type) {
-      case 'function':
-      case 'method':
-        return { foreground: 12 }
-      case 'class':
-        return { foreground: 11 }
-      case 'variable':
-      case 'property':
-        return { foreground: _readonly ? 21 : 9 }
+      case "function":
+      case "method":
+        return { foreground: 12 };
+      case "class":
+        return { foreground: 11 };
+      case "variable":
+      case "property":
+        return { foreground: _readonly ? 21 : 9 };
       default:
-        return { foreground: 0 }
+        return { foreground: 0 };
     }
-  }
+  };
 
   loadGrammars(monaco, editorInstance);
 };
