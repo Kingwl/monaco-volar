401c7f2a5a3ca124c3a871697bda3e32072ac372
白雾三语 <32354856+baiwusanyu-c@users.noreply.github.com>
2023-06-27 23:57:41 +0800

    feat: added light theme (#23)

--- src/themes/index.ts
+++ src/themes/index.ts
@@ -1,7 +1,9 @@
-import { theme } from './converted'
+import { theme, lightTheme } from './converted'
 
 export async function loadTheme(editor: typeof import('monaco-editor-core').editor) {
-    const themeName = 'vs-code-theme-converted';
-    editor.defineTheme(themeName, theme);
-    return themeName;
+    const themeNameDark = 'vs-code-theme-converted-dark';
+    editor.defineTheme(themeNameDark, theme);
+    const themeNameLight = 'vs-code-theme-converted-light';
+    editor.defineTheme(themeNameLight, lightTheme);
+    return { dark: themeNameDark, light: themeNameLight }
 }
